28671e4c3ba2553cd3930e579bbfe68a06597a19
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-09-23 12:38:42 +0200

    Create a for-loop and function to change the values from the modal and backdrop to display='none'

--- shared.js
+++ shared.js
@@ -1,15 +1,36 @@
 const backdrop = document.querySelector('.backdrop')
 const modal = document.querySelector('.modal')
+
+//Create an array
 const selectPlanButtons = document.querySelectorAll('.plan button')
 
 // console.dir(backdrop)
 // console.dir(selectPlanButtons)
 
+//Loop over the array
 for(let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
         modal.style.display = 'block'
         backdrop.style.display = 'block'
     })
+}
+
+//Exsercise
+
+//Select the button and let the modal screen closable
+// Add event listener to the No button
+
+
+//Select the right button and create an array
+const modalButton = document.querySelectorAll('.modal button')
+console.dir(modalButton)
+
+//Loop over the array
+for (let i = 0; i < modalButton.length; i++) {
+    modalButton[i].addEventListener('click', function() {
+        modal.style.display = 'none'
+        backdrop.style.display = 'none'
+    })    
 }
 
 
